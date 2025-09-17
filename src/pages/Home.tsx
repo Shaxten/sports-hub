e9b480136c7228c6177e28312b23fac3e567e835
Shaxten <joeycadoo@hotmail.com>
2025-09-17 09:34:55 -0400

    testupdate

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -5,6 +5,7 @@ export default function Home() {
     <div>
       <h1>Welcome to Sports Hub</h1>
       <p>Your one-stop destination for all things sports!</p>
+      <p>Use the navigation links to explore teams and players.</p>
     </div>
 
   );
